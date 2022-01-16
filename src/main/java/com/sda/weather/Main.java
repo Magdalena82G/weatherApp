59222f8974951c0dc2d51c2f48f31561af3b1a35
Michal Paukszto <michal.paukszto@gmail.com>
2022-01-16 14:55:48 +0100

    test commit

--- src/main/java/com/sda/weather/Main.java
+++ src/main/java/com/sda/weather/Main.java
@@ -3,5 +3,6 @@ package com.sda.weather;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hej");
+        System.out.println("Michal tu byl");
     }
 }
